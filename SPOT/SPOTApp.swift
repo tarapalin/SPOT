81624fd72ffa88da0953926f2cb49757388afbec
tarapalin <tpalin95@gmail.com>
2023-04-21 12:22:59 -0500

    Initial Commit

---
+++ SPOT/SPOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPOTApp.swift
+//  SPOT
+//
+//  Created by Tara Palin on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SPOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
