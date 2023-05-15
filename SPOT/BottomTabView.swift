221944cd976e056d7cc6de3169ed5a74c4664329
aaronpnguyen <aaronnguyen1123@gmail.com>
2023-05-14 23:54:52 -0500

    tab system, needs sf symbols

---
+++ SPOT/BottomTabView.swift
@@ -0,0 +1,51 @@
+//
+//  TabView.swift
+//  SPOT
+//
+//  Created by Aaron Nguyen on 5/14/23.
+//
+
+import SwiftUI
+
+enum TabTitles: String, CaseIterable {
+    case house, map, bell, person
+}
+
+struct BottomTabView: View {
+    @Binding var selectedTab: TabTitles
+    
+    private var filledTab: String {
+        selectedTab.rawValue + ".fill"
+    }
+
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(TabTitles.allCases, id: \.self) { currentTab in
+                    Spacer()
+                    
+                    Image(systemName:
+                        currentTab == selectedTab ? filledTab: currentTab.rawValue
+                    )
+                    .scaleEffect(currentTab == selectedTab ? 1.25 : 1.0)
+                    .font(.system(size: 20))
+                    .onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.1)) {
+                            selectedTab = currentTab
+                        }
+                    }
+                    
+                    Spacer()
+                }
+            }
+            .frame(width: nil, height: 50)
+            .background(.thinMaterial)
+        }
+    }
+}
+
+struct BottomTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomTabView(selectedTab: .constant(.house))
+    }
+}
