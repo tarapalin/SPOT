f8e31e2e5c18468d0d80ed0e0d96603e61c67894
Aaron Nguyen <104883016+aaronpnguyen@users.noreply.github.com>
2023-05-20 08:43:11 -0500

    Setting up tab system (#1)
    * tab system, needs sf symbols
    
    * Colors have been added

--- SPOT/ContentView.swift
+++ SPOT/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelected: TabTitles = .house
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                TabView(selection: $tabSelected) {
+                    ForEach(TabTitles.allCases, id: \.rawValue) { currentTab in
+                        HStack {
+                            Image(systemName: currentTab.rawValue)
+                            Text("\(currentTab.rawValue.capitalized)")
+                                .bold()
+                                .animation(nil, value: tabSelected)
+                        }
+                        .tag(currentTab)
+                    }
+                }
+            }
+            
+            VStack {
+                Spacer()
+                BottomTabView(selectedTab: $tabSelected)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
